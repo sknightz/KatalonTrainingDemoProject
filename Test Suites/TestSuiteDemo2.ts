5fb01108215c7aef64f6751f417d3241b1566cf8
restu <restu@LAPTOP-MCP8DQTB>
2021-03-20 16:54:44 +0700

    added TestSuiteDemo2

---
+++ Test Suites/TestSuiteDemo2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteDemo2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cb91b5b4-980b-45ba-818d-6a27dd05f345</testSuiteGuid>
+   <testCaseLink>
+      <guid>8e99a8be-5c6f-4dfd-b447-12a00a123100</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/RecordModeDemo</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
